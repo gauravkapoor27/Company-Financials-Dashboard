d72066420756c5e69df4354896c654842a759537
Gaurav Kapoor <gaurav.kapoor27@outlook.com>
2023-01-08 20:02:57 +1300

    changed selected style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,20 +8,32 @@ const Header = ({ data, setData, setTicker, companies }) => {
         {data ? (
           <>
             <Select
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 10,
+                colors: {
+                  ...theme.colors,
+                  primary25: "lightblue",
+                  primary: "black",
+                },
+              })}
               styles={{
                 container: (provided) => ({
                   ...provided,
                   position: "absolute",
                   top: "50px",
-                  right: "50px",
-                  width: "400px",
-                  height: "10px",
-                  fontSize: "0.8rem",
+                  right: "70px",
+                  width: "500px",
+                  fontSize: "1.5rem",
                   color: "black",
                 }),
                 menu: (provided) => ({
                   ...provided,
-                  top: "35px",
+                  top: "45px",
+                  right: "0px",
+                  width: "500px",
+                  fontSize: "1.5rem",
+                  overflow: "hidden",
                 }),
               }}
               className="ticker-select"
